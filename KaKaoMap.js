a021a373f88b8e01c7144c4516702c83070495ce
Sanghun-Jin <sanghun1425@naver.com>
2020-07-03 10:17:48 +0900

    asd

--- KaKaoMap.js
+++ KaKaoMap.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {View, Text, StyleSheet, Dimensions } from "react-native";
 import PropTypes from "prop-types";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 const API_KEY = "1e01caddfd42eef0c0788411547967f4";
 
@@ -9,7 +9,21 @@ export default function Viewmap({lati, long}){
     
     return <View style ={ styles.container }>
         <View style = {styles.mapStyle}>
-            <MapView style = {styles.map} />
+            <MapView style = {styles.map}
+                initialRegion = {{latitude: lati, longitude: long, latitudeDelta: 0.0922, longitudeDelta: 0.0421}}
+                showsUserLocation
+                followsUserLocation
+            >
+                <Marker
+                    coordinate={{
+                        latitude: lati,
+                        longitude: long
+                    }}
+                    //image={require('assets/Map_Marker.png')}
+                    title="미디어코어"
+                    description="회사"
+                />
+            </MapView>
         </View>
         <View style = {styles.text}>
             <Text>{lati}</Text>
@@ -32,10 +46,11 @@ const styles = StyleSheet.create({
         flex: 1
     },
     mapStyle: {
+        marginTop: 50,
         flex: 3
     },
     map: {
-        width: 1000,
-        height: 500
+        width: 300,
+        height: 400
     }
 })
